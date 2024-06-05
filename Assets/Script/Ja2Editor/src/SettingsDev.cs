f7b01e4dd5a11c8f0e0b91d57822e74c16161258
Trigve Siver <trigves@yahoo.com>
2024-06-05 19:06:59 +0200

    ja2: Add SettingsDev class

---
+++ Assets/Script/Ja2Editor/src/SettingsDev.cs
@@ -0,0 +1,90 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Ja2.Editor
+{
+	/// <summary>
+	/// Settings used while developement.
+	/// </summary>
+	public sealed class SettingsDev : ScriptableObject
+	{
+#region Fields
+		/// <summary>
+		/// Directory used for imports (.slf, ...)
+		/// </summary>
+		public string m_InputDir = string.Empty;
+
+		/// <summary>
+		/// Data directory used.
+		/// </summary>
+		public string m_DataDir = string.Empty;
+#endregion
+
+#region Fields Static
+		/// <summary>
+		/// Static instance.
+		/// </summary>
+		private static SettingsDev? m_Instance;
+#endregion
+
+#region Properties Static
+		/// <summary>
+		/// Get current instance.
+		/// </summary>
+		/// <exception cref="FileNotFoundException"></exception>
+		public static SettingsDev instance
+		{
+			get
+			{
+
+				// Not loaded yet
+				if(m_Instance is null)
+				{
+					string[] assets_found = AssetDatabase.FindAssets("t:SettingsDev");
+					if(assets_found.Length != 0)
+					{
+						m_Instance = AssetDatabase.LoadMainAssetAtPath(
+							AssetDatabase.GUIDToAssetPath(assets_found[0])
+						) as SettingsDev;
+					}
+				}
+
+				// Still not found
+				if(m_Instance is null)
+					throw new FileNotFoundException("Cannor find dev settings asset.");
+
+				return m_Instance;
+			}
+		}
+#endregion
+
+#region Methods Static
+		/// <summary>
+		/// Context menu asset creation.
+		/// </summary>
+		[MenuItem("Assets/Create/JA2 Devel Settings", false, 1)]
+		private static void CreateContext()
+		{
+			ProjectWindowUtil.CreateAsset(Create(),
+				"Ja2SettingsDev.asset"
+			);
+		}
+#endregion
+
+#region Construction
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <returns>New instance.</returns>
+		private static SettingsDev Create()
+		{
+			var ret = CreateInstance<SettingsDev>();
+			ret.m_InputDir = Application.streamingAssetsPath;
+			ret.m_DataDir = "Assets/Data";
+
+			return ret;
+		}
+#endregion
+	}
+}
