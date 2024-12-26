19ee055e39434736e6c9498cc89e389451c22881
Trigve Siver <trigves@yahoo.com>
2024-12-26 18:02:19 +0100

    script: Process command line arguments in GameManager

--- Assets/Script/Ja2Core/src/GameManager.cs
+++ Assets/Script/Ja2Core/src/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 using UnityEngine;
 
 namespace Ja2
@@ -12,6 +15,42 @@ namespace Ja2
 		public void Start()
 		{
 			RandomManager.Init();
+
+			ProcessJa2CommandLineBeforeInitialization();
+		}
+#endregion
+
+#region Methods
+		/// <summary>
+		/// Process JA2 command line before initializtion is done.
+		/// </summary>
+		private void ProcessJa2CommandLineBeforeInitialization()
+		{
+			// Get all arguments
+			foreach(string token_upper in Environment.GetCommandLineArgs().Select(Token => Token.ToUpperInvariant()))
+			{
+				// "NO SOUND" option
+				if(token_upper == "/NOSOUND")
+				{
+					// Disable the sound
+					Ja2Settings.isSoundEnabled = false;
+				}
+				else if(token_upper == "/FULLSCREEN")
+				{
+					// Overwrite Graphic setting from JA2_settings.ini
+					Ja2Settings.windowMode = Ja2Settings.WindowMode.Fullscreen;
+					Ja2Settings.cmdWindowMode = true;
+
+					// no resolution read from Args. Still from INI, but could be added here, too...
+				}
+				else if(token_upper == "/WINDOW")
+				{
+					// Overwrite Graphic setting from JA2_settings.ini
+					Ja2Settings.windowMode = Ja2Settings.WindowMode.Windowed;
+					Ja2Settings.cmdWindowMode = true;
+					// \TODO No resolution read from Args. Still from INI, but could be added here, too...
+				}
+			}
 		}
 #endregion
 	}
