849986835424a94fb7d539307fec72d5a4406a69
Trigve Siver <trigves@yahoo.com>
2025-12-28 14:30:51 +0100

    script: Create the AssetBundleDesc for each .slf extracted

--- Assets/Script/Ja2Editor/src/EditorMenu.cs
+++ Assets/Script/Ja2Editor/src/EditorMenu.cs
@@ -19,6 +19,34 @@ namespace Ja2.Editor
 			// Read all the SLF
 			foreach(string it in Directory.EnumerateFiles(SettingsDev.instance.m_InputDir, "*.slf"))
 			{
+				// Bundle name
+				string? bundle_name = UtilsPath.NormalizePath(
+					Path.GetFileNameWithoutExtension(it)
+				);
+
+				// Bundle path
+				string bundle_dir = UtilsPath.Combine(SettingsDev.instance.m_SlfExtractDir,
+					bundle_name
+				);
+
+				// Be sure directory exist
+				if(!Directory.Exists(bundle_dir))
+					Directory.CreateDirectory(bundle_dir);
+
+				// Create the asset bundle descriptor
+				var bundle_desc = AssetBundleDesc.Create(1,
+					(uint)bundle_name.GetHashCode(),
+					bundle_name,
+					bundle_name + ".bundle"
+				);
+
+				// Save the asset bundle descriptor to dir where all the files will be extracted
+				AssetDatabase.CreateAsset(bundle_desc,
+					UtilsPath.Combine(bundle_dir,
+						AssetBundleDesc.FileName
+					)
+				);
+
 				// As first SLF
 				foreach(FileData file_data in SlfManager.ExtractPath(it))
 				{
